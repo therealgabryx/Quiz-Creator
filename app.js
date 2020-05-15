461688dbaf1a0460b1ed4a2dbe3c85db2aca7bd8
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-15 19:16:01 +0200

    connecting functions

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 // DOM generated content -- to implement afterwards
+// remembr: onload="firstLoad()" >> on body
 
-/* 
-remembr: onload="firstLoad()" >> on body
 
-function firstLoad() {
-    var par = document.createElement('P');
-    var sampletext = document.createTextNode('this is a sample DOM-generated text')
-    par.appendChild(sampletext);
-    document.getElementById('heading').appendChild(par);
-}
-*/
+
+
 
 function createQuiz() {
     const heading = document.getElementById('heading')
     const bodyContent = document.getElementById('body-content')
     const footer = document.getElementById('footer')
 
-    bodyContent.innerHTML = ''
+    bodyContent.innerHTML = '<form id="quizNameForm" name="quizNameForm" onsubmit="return false"><br>' +
+                                '<label for="quizName">Enter a quiz name:</label><br>' +
+                                '<input type="text" id="quizName" name="quizName"><br><br>' +
+                                '<p id="errorBox" name="errorBox"></p><br>' +
+                                '<button type="submit" id="insertName--continue" name="insertName--continue" onclick="checkQuizName()">Continue</button>' +
+                            '</form>' 
 }
 
 function checkQuizName() {
@@ -30,7 +29,24 @@ function checkQuizName() {
         document.getElementById('errorBox').innerHTML = 'insert a valid name'
     }
     else {
-        bodyContent.innerHTML = ''
+        bodyContent.innerHTML = '<form id="inputQuestionForm" name="inputQuestionForm" onsubmit="return false">' +
+                                    '<label for="question">Enter a question:</label><br>' +
+                                    '<input type="text" id="question" name="question"><br><br>'
+                                    '<p id="errorBoxQuestion" name="errorBoxQuestion"></p>'
+
+                                    '<label for="options">Options:</label>'
+                                    '<fieldset id="options">'
+                                        '<label for="textOpt1">A.</label><input type="text" id="textOpt1" name="textOpt1"><input type="checkbox" id="checkOpt1" name="checkOpt1"><br>'
+                                        '<label for="textOpt2">B.</label><input type="text" id="textOpt2" name="textOpt2"><input type="checkbox" id="checkOpt2" name="checkOpt2"><br>'
+                                        <label for="textOpt3">C.</label><input type="text" id="textOpt3" name="textOpt3"><input type="checkbox" id="checkOpt3" name="checkOpt3"><br>
+                                        <label for="textOpt4">D.</label><input type="text" id="textOpt4" name="textOpt4"><input type="checkbox" id="checkOpt4" name="checkOpt4"><br>
+                                    '</fieldset>'
+
+                                    '<p id="errorBoxAnswers" name="errorBoxAnswers"></p><br>'
+
+                                    <button type="submit" id="inputQuestion--continue" name="inputQuestion--continue" onclick="checkQuestionInput()">Add another question</button>
+                                    <button type="submit" id="inputQuestion--end" name="inputQuestion--end" onclick="endQuestionInput()">End Questions Input</button>
+                                '</form>'
     }
 }
 
